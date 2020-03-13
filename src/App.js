e54aee378bb6dedb5bba403f1e6919f09aafc3fb
ravi rathore <ravirathore15@icloud.com>
2020-03-13 14:42:21 +1100

    added hook

--- src/App.js
+++ src/App.js
@@ -2,25 +2,68 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+function List(props)
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      {
+        props.stories.map((item) => <div key={item.objectID}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span
+          ></div>)
+      }
     </div>
   );
 }
+const App = () => {
+
+const stories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan walke',
+    num_comments: 3,
+    poits: 4,
+    ojgectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan abramov, Andrew clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'fart theory',
+    url: 'https://redux.js.org',
+    author: 'Ravi',
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  }
+];
+
+const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+return(
+  <div>
+    <h1>My Hacker Stories</h1>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={handleChange}></input>
+    <hr />
+    <List stories={stories} />
+  </div>
+);
+};
 
 export default App;
